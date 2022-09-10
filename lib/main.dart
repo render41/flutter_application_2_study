3d026f53fd15924740f508deae8cc47cbe493ec0
Gabriel Miranda <gabrielservice05@gmail.com>
2022-09-10 09:13:48 -0300

    TEste

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp(
-    title: 'Welcome',
-  ));
-}
+void main() => runApp(const MyApp(
+      title: 'Welcome',
+      color: Color.fromARGB(255, 134, 133, 133),
+    ));
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.title});
+  const MyApp({super.key, required this.title, required this.color});
 
   final String title;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: title,
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 134, 133, 133),
+        backgroundColor: color,
         appBar: AppBar(
-          title: const Text('Hello Fernanda'),
+          title: const Text('Hello Word'),
           backgroundColor: const Color.fromARGB(255, 58, 58, 58),
         ),
         body: const Center(
